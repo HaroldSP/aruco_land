f00b932601c2c0bddca124343450250bb2fe5089
HaroldSP <George.St.Petersburg@gmail.com>
2021-06-02 01:48:08 +0300

    2021-06-02_01:48:08

--- aruco_func_test.py
+++ aruco_func_test.py
@@ -116,19 +116,19 @@ def lander():
             print(x_ang,y_ang)
             print(-x_ang_control,-y_ang_control)
             print("########################################################")
-            cap.stream.release()
+            cap.release()
             time.sleep(5)
             return None
         else:
             notfound_count=notfound_count+1
             print("FOUND COUNT: "+str(found_count)+" NOTFOUND COUNT: "+str(notfound_count))
-            cap.stream.release()
+            cap.release()
             time.sleep(10)
             print("waited 5 sec to release cam")
             return None
     except Exception as e:
         print('Target likely not found. Error: '+str(e))
         notfound_count=notfound_count+1
-        cap.stream.release()
+        cap.release()
         time.sleep(5)
         return None
