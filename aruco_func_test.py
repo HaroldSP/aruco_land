c1161de7ff920157c726902c4addca9c684dec5c
HaroldSP <George.St.Petersburg@gmail.com>
2021-06-02 10:05:22 +0300

    2021-06-02_10:05:21

--- aruco_func_test.py
+++ aruco_func_test.py
@@ -20,33 +20,8 @@ from simple_pid import PID
 #########FUNCTIONS#################
 
 def lander():
-
+    global first_run, notfound_count, found_count, marker_size, start_time
     global p, i, d, pid, marker_size
-
-    ##Counters and script triggers
-    found_count=0
-    notfound_count=0
-
-    first_run=0 #Used to set initial time of function to determine FPS
-    start_time=0
-    end_time=0
-
-    #####Camera#######
-    horizontal_res = 640
-    vertical_res = 480
-    cap = WebcamVideoStream(src=0, width=horizontal_res, height=vertical_res).start()
-
-    ##################
-    horizontal_fov = 62.2 * (math.pi / 180)  # Pi cam V1: 53.5 V2: 62.2
-    vertical_fov = 48.8 * (math.pi / 180)  # Pi cam V1: 41.41 V2: 48.8
-    ##################
-    calib_path = "/home/pi/video2calibration/calibrationFiles/"
-    cameraMatrix = np.loadtxt(calib_path+'cameraMatrix.txt', delimiter=',')
-    cameraDistortion = np.loadtxt(calib_path+'cameraDistortion.txt', delimiter=',')
-    ##################
-    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
-    parameters = aruco.DetectorParameters_create()
-    ##################
 
     if first_run==0:
         print("First run of lander!!")
