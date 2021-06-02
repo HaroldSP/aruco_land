0359569eca156c458fd715f44cf3a381adaae633
HaroldSP <George.St.Petersburg@gmail.com>
2021-06-02 23:21:54 +0300

    2021-06-02_23:21:53

--- gitpullandpush.zsh
+++ gitpullandpush.zsh
@@ -7,3 +7,7 @@ git pull
 git add .
 git commit -m "$NOW"
 git push -u origin master 
+
+#делаем принудительный коммит в основной репо на гитхабе
+#git push -f origin master
+#без -f будет ругаться что у вас версия младше чем в гитхабе и вам надо сделать pull
--- gitpush.zsh
+++ gitpush.zsh
@@ -5,3 +5,7 @@ NOW=$( date '+%F_%H:%M:%S' )
 git add .
 git commit -m "$NOW"
 git push -u origin master 
+
+#делаем принудительный коммит в основной репо на гитхабе
+#git push -f origin master
+#без -f будет ругаться что у вас версия младше чем в гитхабе и вам надо сделать pull
