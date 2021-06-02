445a22c854c2d5b03f8d3e2442a603c3c957ffe4
HaroldSP <George.St.Petersburg@gmail.com>
2021-06-02 23:24:49 +0300

    checking_pass_arg

--- gitpullandpush.zsh
+++ gitpullandpush.zsh
@@ -5,9 +5,13 @@ NOW=$( date '+%F_%H:%M:%S' )
 git pull
 
 git add .
-git commit -m "$NOW"
+#git commit -m "$NOW"
+git commit -m "$1"
 git push -u origin master 
 
 #делаем принудительный коммит в основной репо на гитхабе
 #git push -f origin master
 #без -f будет ругаться что у вас версия младше чем в гитхабе и вам надо сделать pull
+
+#passing arguments to a bash script:
+#https://www.futurelearn.com/info/courses/linux-for-bioinformatics/0/steps/202962
