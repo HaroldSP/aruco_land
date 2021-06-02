03ad05e79c97e76a5c1bccad1b9037c6d42c58c2
HaroldSP <George.St.Petersburg@gmail.com>
2021-06-02 23:29:21 +0300

    2021-06-02_23:29:21 ___ checking

--- test_printtest.py
+++ test_printtest.py
@@ -6,6 +6,7 @@
 #WORKING ON RASPBERRY AND NAVIO2 !!!
 
 #checking .gitignore 
+#checking passing commit name as arg to a bash script
 
 ##########DEPENDENCIES################
 #import aruco_func as AR           # my custom imports
