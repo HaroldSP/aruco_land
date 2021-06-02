eb962a561885fbb43c25509facac17169baac3e8
HaroldSP <George.St.Petersburg@gmail.com>
2021-06-02 16:05:52 +0300

    2021-06-02_16:05:51

--- aruco_func_test.py
+++ aruco_func_test.py
@@ -116,19 +116,11 @@ def lander():
             print(x_ang,y_ang)
             print(-x_ang_control,-y_ang_control)
             print("########################################################")
-            cap.stream.release()
-            time.sleep(5)
-            return None
+            break
         else:
             notfound_count=notfound_count+1
             print("FOUND COUNT: "+str(found_count)+" NOTFOUND COUNT: "+str(notfound_count))
-            cap = WebcamVideoStream(src=0, width=horizontal_res, height=vertical_res).release()
-            time.sleep(10)
-            print("waited 5 sec to release cam")
-            return None
+            break
     except Exception as e:
         print('Target likely not found. Error: '+str(e))
-        notfound_count=notfound_count+1
-        cap.stream.release()
-        time.sleep(5)
-        return None
+        break
