b8bee25f32eedc3e0becdb8820fbfd6dfb3287d5
HaroldSP <George.St.Petersburg@gmail.com>
2021-06-03 15:14:57 +0300

    2021-06-03_15:14:57 _ added_fly_test_in_separate_folder

--- fly_tests_3_06/test_printtest.py
+++ fly_tests_3_06/test_printtest.py
@@ -3,16 +3,11 @@
 
 #THIS IS A NEW TEST SCRIPT RUNNING SEVERAL FUNCSIONS FROM MODULES
 #PRINT DEBUG VERSION
-#WORKING ON RASPBERRY AND NAVIO2 !!!
-
-#checking .gitignore 
-#checking passing commit name as arg to a bash script!!
 
 ##########DEPENDENCIES################
-#import aruco_func as AR           # my custom imports
-#import aruco_func_test as AR       # my custom imports
 import functions as F              # my custom imports
-import multicam as M
+import multicam as M               # my custom imports
+#import sys                         # this import is for passing arguments to a python script
 
 import argparse
 import cv2
@@ -31,6 +26,14 @@ from pymavlink import mavutil
 from simple_pid import PID
 
 #######VARIABLES####################
+
+#######these are for passing args to a py script########################
+
+#firstarg = int(sys.argv[1])
+#secondarg = sys.argv[2]
+# thirdarg = sys.argv[3]
+
+
 id_to_find = 72 ##Aruco
 marker_size = 20 #cm
 
@@ -55,24 +58,25 @@ end_time = 0
 horizontal_res = 640
 vertical_res = 480
 
-# cap = WebcamVideoStream(src=0, width=horizontal_res, height=vertical_res).start()
-# cap = WebcamVideoStream(src=0).start()
-# cap.set(3,640) #Setting webcam's image width 
-# cap.set(4,480) #Setting webcam' image height
 ##################
 horizontal_fov = 62.2 * (math.pi / 180)  # Pi cam V1: 53.5 V2: 62.2
 vertical_fov = 48.8 * (math.pi / 180)  # Pi cam V1: 41.41 V2: 48.8
 ##################
+
+#######comment that section 1 to run on PC#########
 calib_path = "/home/pi/video2calibration/calibrationFiles/"
 cameraMatrix = np.loadtxt(calib_path+'cameraMatrix.txt', delimiter=',')
 cameraDistortion = np.loadtxt(calib_path+'cameraDistortion.txt', delimiter=',')
-##################
+##################11111111111111111############
+
 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
 parameters = aruco.DetectorParameters_create()
 ##################
-vs = M.WebcamVideoStream().start()
-#########FUNCTIONS#################
 
+#starting videostream here, before the loop
+vs = M.WebcamVideoStream().start()
+
+#########LANDER FUNCTION#################
 
 def lander():
     global first_run, notfound_count, found_count, marker_size, start_time, vs
@@ -82,12 +86,7 @@ def lander():
         print("First run of lander!!")
         first_run = 1
         start_time = time.time()
-    #print("does this happens?")
-    #frame = cap.read()
-    #vs = M.WebcamVideoStream().start()
     frame = vs.read()
-    #cv2.imshow('webcam', frame)
-    #print(frame)
     frame = cv2.resize(frame, (horizontal_res, vertical_res))
     frame_np = np.array(frame)
     gray_img = cv2.cvtColor(frame_np, cv2.COLOR_BGR2GRAY)
@@ -95,14 +94,12 @@ def lander():
     corners, ids, rejected = aruco.detectMarkers(image=gray_img, dictionary=aruco_dict, parameters=parameters)
 
     print("FIRST TIME LAND COMMAND - UNCOMMENT LATER!")
-    print("DRONE IS IN THE LAND MODE")
+    print("DRONE IS IN THE LAND MODE - checking in loop")
     # if vehicle.mode!='LAND':
     #     vehicle.mode=VehicleMode("LAND")
     #     while vehicle.mode!='LAND':
     #         print('WAITING FOR DRONE TO ENTER LAND MODE')
     #         time.sleep(1)
-
-    print('prepare to start TRY block')
     try:
         print('started TRY block')
         if ids is not None and ids[0] == id_to_find:
@@ -158,97 +155,87 @@ def lander():
         print('Target likely not found. Error: '+str(e))
         return None
 
-##########MAIN##############
-try:
-    vehicle = F.connectMyCopter()
-    while vehicle != vehicle:
-        print("Waiting for vehicle to connect to mavlink.")
-        time.sleep(1)
+if __name__ == '__main__':
 
-    ##
-    ##SETUP PARAMETERS TO ENABLE PRECISION LANDING
-    ##
-    vehicle.parameters['PLND_ENABLED'] = 1
-    vehicle.parameters['PLND_TYPE'] = 1 ##1 for companion computer
-    vehicle.parameters['PLND_EST_TYPE'] = 0 ##0 for raw sensor, 1 for kalman filter pos estimation
-    vehicle.parameters['LAND_SPEED'] = 20 ##Descent speed of 20cm/s
+    try:
+        ##########comment that section 2 to run on PC#########
 
-    #DEBUG SECTION 1#
-    print("this is land speed", vehicle.parameters['LAND_SPEED'])
-    print(vehicle.parameters['LAND_SPEED'])
-    #################
-
-    if script_mode ==1:
-        #F.arm_and_takeoff(takeoff_height)
-        ############2#######################
-        print("FLYING UP!!", takeoff_height)
-        ####################################
-        #print(str(time.time()))
-        today = datetime.datetime.today()
-        print(today.strftime("%Y-%m-%d-%H.%M.%S"))  # 2017-04-05-00.18.00
-        #F.send_local_ned_velocity(velocity,velocity,0) ##Offset drone from target
-        time.sleep(1)
-        ready_to_land=1
-        ############3#######################
-        print("READY TO LAND! Should be 1 =", ready_to_land)
-        ####################################
-    elif script_mode==2:
-        while vehicle.mode!='GUIDED':
+        vehicle = F.connectMyCopter()
+        while vehicle != vehicle:
+            print("Waiting for vehicle to connect to mavlink.")
             time.sleep(1)
-            print("Waiting for manual change from mode "+str(vehicle.mode)+" to GUIDED")
-        ready_to_land=1
+        print("Successfully connected to vehicle")
+        #
+        #SETUP PARAMETERS TO ENABLE PRECISION LANDING
+        #
+        vehicle.parameters['PLND_ENABLED'] = 1
+        vehicle.parameters['PLND_TYPE'] = 1 ##1 for companion computer
+        vehicle.parameters['PLND_EST_TYPE'] = 0 ##0 for raw sensor, 1 for kalman filter pos estimation
+        vehicle.parameters['LAND_SPEED'] = 20 ##Descent speed of 20cm/s
 
-    if ready_to_land==1:
-        ############4#######################
-        print("ENTERING ready_to_land SECTION")
-        ARMED = 0
-        print("ARMED:", ARMED)
-        ####################################
-        #while vehicle.armed==True:
-        #while ARMED != 10:
-        while True:
-            lander()
+        ######################22222222222222222##############
+
+        if script_mode ==1:
+            #F.arm_and_takeoff(takeoff_height)
+            ############2#######################
+            print("FLYING UP!!", takeoff_height)
+            ####################################
+            today = datetime.datetime.today()
+            print(today.strftime("%Y-%m-%d-%H.%M.%S"))  # 2017-04-05-00.18.00 format
+            #F.send_local_ned_velocity(velocity,velocity,0) ##Offset drone from target
             time.sleep(1)
-            ARMED = ARMED + 1
+            ready_to_land=1
+
+        elif script_mode==2:
+            while vehicle.mode!='GUIDED':
+                time.sleep(1)
+                print("Waiting for manual change from mode "+str(vehicle.mode)+" to GUIDED")
+            ready_to_land=1
+
+        if ready_to_land==1:
+            ############4#######################
+            print("ENTERING ready_to_land SECTION in MAIN")
+            ARMED = 0
             print("ARMED:", ARMED)
-            #frame = vs.read()
-            #cv2.imshow('webcam', frame)
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     vs.stop()
-            #     break
-            #vs.stop()
-            #cv2.destroyAllWindows()
+            ####################################
+            #while vehicle.armed==True:
+            #while ARMED != 10:
+            while True:
+                lander()
+                time.sleep(1)
+                ARMED = ARMED + 1
+                print("ARMED:", ARMED)
+                #break that loop by ctrl+C
 
-        # print('waited 10 sec to land')
-        # print('LANDED')
-        # end_time = time.time()
-        # total_time=end_time-start_time
-        # total_time=abs(int(total_time))
+            # print('waited 10 sec to land')
+            # print('LANDED')
+            # end_time = time.time()
+            # total_time=end_time-start_time
+            # total_time=abs(int(total_time))
 
-        # total_count=found_count+notfound_count
-        # freq_lander=total_count/total_time
-        # print("Total iterations: "+str(total_count))
-        # print("Total seconds: "+str(total_time))
-        # print("------------------")
-        # print("lander function had frequency of: "+str(freq_lander))
-        # print("------------------")
-        # print("Vehicle has landed")
-        # print("------------------")
-except KeyboardInterrupt:
-    vs.stop()
-    print("THIS IS THE END, LANDED")
-    end_time = time.time()
-    total_time=end_time-start_time
-    total_time=abs(int(total_time))
+            # total_count=found_count+notfound_count
+            # freq_lander=total_count/total_time
+            # print("Total iterations: "+str(total_count))
+            # print("Total seconds: "+str(total_time))
+            # print("------------------")
+            # print("lander function had frequency of: "+str(freq_lander))
+            # print("------------------")
+            # print("Vehicle has landed")
+            # print("------------------")
+    except KeyboardInterrupt:
+        vs.stop()
+        print("THIS IS THE END, LANDED")
+        end_time = time.time()
+        total_time=end_time-start_time
+        #total_time=abs(int(total_time))
 
-    total_count=found_count+notfound_count
-    freq_lander=total_count/total_time
-    print("Total iterations: "+str(total_count))
-    print("Total seconds: "+str(total_time))
-    print("------------------")
-    print("lander function had frequency of: "+str(freq_lander))
-    print("------------------")
-    print("Vehicle has landed")
-    print("------------------")
-
-    pass
+        total_count=found_count+notfound_count
+        freq_lander=abs(float(total_count/total_time))
+        print("Total iterations: "+str(total_count))
+        print("Total seconds: "+str(total_time))
+        print("------------------")
+        print("lander function had frequency of: "+str(freq_lander))
+        print("------------------")
+        print("Vehicle has landed")
+        print("------------------")
+        pass
